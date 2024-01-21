8b54ddfe720badf9ab567a7fdf4dbd70db31eb2b
dishavirk <disha.20.10@gmail.com>
2024-01-22 00:03:52 +0100

    adding fahrenheit to celsius conversion

--- celsius-to-fahrenheit.py
+++ celsius-to-fahrenheit.py
@@ -1,30 +1,54 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return (
+        """<form action="/to_fahrenheit" method="get">
+                <h1>Convert Celsius to Fahrenheit:</h1> 
+                <input type="text" name="celsius">
+                <input type="submit" value="Convert">
+            </form>
+            <br>
+            <form action="/to_celsius" method="get">
+                <h1>Convert Fahrenheit to Celsius:</h1> 
+                <input type="text" name="fahrenheit">
+                <input type="submit" value="Convert">
+            </form>"""
+    )
+
+@app.route("/to_fahrenheit")
+def to_fahrenheit():
     celsius = request.args.get("celsius", "")
     if celsius:
         fahrenheit = fahrenheit_from(celsius)
     else:
-        fahrenheit = ""
-    return (
-        """<form action="" method="get">
-                <h1>Celsius temperature: </h1> <input type="text" name="celsius">
-                <input type="submit" value="Convert to Fahrenheit">
-            </form>"""
-        + "<h1>Fahrenheit:</h1> "
-        + fahrenheit
-    )
+        fahrenheit = "<h1>Please enter a value</h1>"
+    return fahrenheit
+
+@app.route("/to_celsius")
+def to_celsius():
+    fahrenheit = request.args.get("fahrenheit", "")
+    if fahrenheit:
+        celsius = celsius_from(fahrenheit)
+    else:
+        celsius = "<h1>Please enter a value</h1>"
+    return celsius
 
 def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
     try:
         fahrenheit = float(celsius) * 9 / 5 + 32
         fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return "<h1>"+str(fahrenheit)+"</h1>"
+        return f"<h1>{fahrenheit}°F</h1>"
+    except ValueError:
+        return "<h1>Invalid input</h1>"
+
+def celsius_from(fahrenheit):
+    try:
+        celsius = (float(fahrenheit) - 32) * 5 / 9
+        celsius = round(celsius, 3)  # Round to three decimal places
+        return f"<h1>{celsius}°C</h1>"
     except ValueError:
         return "<h1>Invalid input</h1>"
 
